f0c06ed03ac9a6dad2538a37d23044659e08d2a3
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2022-02-14 15:19:36 -0300

    Implementing application logic

--- app/src/main/java/com/rafael/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/rafael/calculadoradenotas/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.rafael.calculadoradenotas
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular = bt_calcular
+        val result = resultado
+
+        btCalcular.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2) / 2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if(media >= 6 && faltas <= 10) {
+                result.text = "Aluno foi Aprovado\nNota final: $media\nFaltas: $faltas"
+                result.setTextColor(Color.GREEN)
+            } else {
+                result.text = "Aluno foi Reprovado\nNota final: $media\nFaltas: $faltas"
+                result.setTextColor(Color.RED)
+            }
+        }
     }
 }
